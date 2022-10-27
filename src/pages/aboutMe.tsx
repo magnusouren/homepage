53b69ff1e029a271ec78753ac0da11bbeab9754f
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2022-10-27 22:25:59 +0200

    Add aboutMe page

---
+++ src/pages/aboutMe.tsx
@@ -0,0 +1,9 @@
+import "../App.css";
+import { CvPhoto } from "../components/home/cvphoto";
+
+export const AboutMe = () => (
+    <div>
+        <h2 className="m-4 text-2xl">This is me</h2>
+        <CvPhoto />
+    </div>
+);
