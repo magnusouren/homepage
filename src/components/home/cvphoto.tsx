c5822389a95c03562f9bb115813e547b99a9a9db
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2022-10-27 22:23:12 +0200

    Refactor to lambda expression

--- src/components/home/cvphoto.tsx
+++ src/components/home/cvphoto.tsx
@@ -1,14 +1,5 @@
-import React from "react";
 import CVImageToAdd from "../../img/cvphoto.jpg";
 
-const CvPhoto = () => {
-    return (
-        <img
-            alt="Monaco swimminpool"
-            src={CVImageToAdd}
-            className="w-60 absolute left-80 top-64 "
-        ></img>
-    );
-};
-
-export default CvPhoto;
+export const CvPhoto = () => (
+    <img alt="CV photo of me" src={CVImageToAdd} className="w-60 m-4"></img>
+);
