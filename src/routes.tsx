4e904fec730e62bf1988f2c5692519fbd2a29b3d
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2022-10-27 22:24:38 +0200

    Add routing to /me

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,13 +4,14 @@ import {
     Route,
 } from "react-router-dom";
 import { PageLayout } from "./layouts/page";
+import { AboutMe } from "./pages/aboutMe";
 import { HomeView } from "./pages/homeview";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<PageLayout />}>
             <Route path="/" element={<HomeView />} />
-            {/* <Route path="/me" element={<HomeView />} /> */}
+            <Route path="/me" element={<AboutMe />} />
         </Route>
     )
 );
