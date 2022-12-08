56e0ac05baf8900a7cfb5405c003dd95936ed16c
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2022-12-08 22:30:35 +0100

    Add content to about me

--- src/pages/aboutMe.tsx
+++ src/pages/aboutMe.tsx
@@ -2,22 +2,45 @@ import { CvPhoto } from '../components/home/cvphoto';
 
 export const AboutMe = () => (
   <div>
-    <div className="w-96 ml-auto mr-auto">
+    <div className="ml-auto mr-auto max-w-xl">
       <h2 className="text-4xl font-bold my-8">about me</h2>
       <p>
-        I am a 20 year old student from Trondheim, Norway. Currently studying informatics at NTNU in Trondheim. At the
-        moment at the second year of the bachelor program. I am a very curious person and I love to learn new things.
+        I am a 22 year old student from Trondheim, Norway. Grew up and comes from Oslo. Currently studying informatics
+        at NTNU in Trondheim. At the moment at the second year of the bachelor program. I am a very curious person and I
+        love to learn new things. Studying informatics makes it posible to learn a lot of new things I never thought
+        would be relevant to learn, but the more I learn the more I understands how much I don't know.
       </p>
       <CvPhoto />
       <p className="my-4">
         Love the sosial aspect of studying in Trondheim, and I am very happy to be a part of the student life here.
         Currently I am a part of the student organization for informatics, called Online. Where I am also a part of the
-        board for the student organization.
+        board for the student organization. My role in the board is to be represant for the Business relation committee.
+        More about Online can be found{' '}
+        <a
+          href="https://online.ntnu.no/"
+          className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+        >
+          here
+        </a>
+        . If you want to know more about me and what I do you should visit the <i className="italic">CV</i> and{' '}
+        <i className="italic">projects</i> page.
       </p>
       <p className="my-4">
-        Currently I am working for Havsport as maintainer of the webshop. I am working with the development of the
-        webshop for the company. Also working with the development of the company website. This is my first job
-        relateded to IT, and gived me a lot of useful experience.
+        Currently I am working for{' '}
+        <a
+          href="https://www.dykkershop.no/"
+          className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+        >
+          Havsport Dykkersenter
+        </a>{' '}
+        as maintainer of the webshop. Also working with the development of the website for the shop. This is my first
+        job relateded to IT, and has gived me a lot of useful experience. Mainly working with Wordpress, but also some
+        other technologies.
+      </p>
+      <p>
+        I am not a big fan of writing about myself, so I will leave it at that. But something that is really cool is
+        that mostly of this content is generated with Github Copilot and I am very impressed with the results. And yes I
+        have made sure that the content is correct.
       </p>
     </div>
   </div>
