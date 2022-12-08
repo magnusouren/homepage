37d73114656db0d1c2746e9f438c990ebba8ae27
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2022-12-08 21:25:52 +0100

    Add routing to projects

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,14 @@ import { createRoutesFromElements, createBrowserRouter, Route } from 'react-rout
 import { PageLayout } from './layouts/page';
 import { AboutMe } from './pages/aboutMe';
 import { HomeView } from './pages/homeview';
+import { Projects } from './pages/projects';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<PageLayout />}>
       <Route path="/" element={<HomeView />} />
       <Route path="/me" element={<AboutMe />} />
+      <Route path="/projects" element={<Projects />} />
     </Route>,
   ),
 );
