033d6f764ef7be9808c313e0f3d39fcbb15c9283
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2022-12-11 00:53:42 +0100

    Add cv

---
+++ src/components/cv/cvJob.tsx
@@ -0,0 +1,24 @@
+export interface CVJobProps {
+  jobTitle: string;
+  company: string;
+  duration: string;
+  jobDescription: string;
+  logo: string;
+}
+
+export const CVJob: React.FC<CVJobProps> = (props) => {
+  return (
+    <div className=" py-4">
+      <div className="flex justify-between">
+        <div className="pr-4">
+          <h4 className="font-semibold my-2">
+            {props.jobTitle} at {props.company}
+          </h4>
+          <span className="text-gray-600 my-2">{props.duration}</span>
+          <p className="text-gray-800 my-2">{props.jobDescription}</p>
+        </div>
+        <img src={props.logo} alt="logo" className="w-36 mt-auto mb-auto m-2" />
+      </div>
+    </div>
+  );
+};
--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -1,5 +1,58 @@
+import { CVJob, CVJobProps } from '../components/cv/cvJob';
+import ForsvaretLogo from '../img/cv/forsvaret.jpg';
+import Havsport from '../img/cv/havsport.svg';
+import NLS from '../img/cv/nls.jpeg';
+
+const CVJobs: CVJobProps[] = [
+  {
+    jobTitle: 'Webshop maintainer and developer',
+    company: 'Havsport Diving center AS',
+    duration: 'January 2022 - Present',
+    jobDescription: 'Maintaining and developing the webshop for Havsport Diving center AS.',
+    logo: Havsport,
+  },
+  {
+    jobTitle: 'Lifeguard',
+    company: 'Norwegian Lifeguard Association',
+    duration: 'June 2022 - August 2022',
+    jobDescription: 'Summer jobb as lifeguard at the public beaches in Oslo.',
+    logo: NLS,
+  },
+  {
+    jobTitle: 'Diving instructor',
+    company: 'The Norwegain Naval Diving School - Royal Norwegian Navy',
+    duration: 'July 2021 - July 2022',
+    jobDescription:
+      'Dive instructor for the Royal Norwegian Navy. Teaching and training skills for all types of divers for the Norwegian Armed Forces.',
+    logo: ForsvaretLogo,
+  },
+  {
+    jobTitle: 'Shipdiver',
+    company: 'The Norwegain Naval Diving School - Royal Norwegian Navy',
+    duration: 'July 2019 - July 2021',
+    jobDescription:
+      'Served as private as shipdiver for the Royal Norwegian Navy. Helping with maintanance and as instructor at the diving school. Teaching and training divers with fundamental skills for becoming shipdivers.',
+    logo: ForsvaretLogo,
+  },
+];
+
 export const CV = () => (
-  <div>
-    <h2 className="m-4 text-2xl">curriculum vitae</h2>
+  <div className="ml-auto mr-auto max-w-xl">
+    <h2 className="text-4xl font-bold my-4">curriculum vitae</h2>
+    <div className="py-4 border-b border-gray-700">
+      <h3 className="text-2xl font-semibold">working experience</h3>
+      {CVJobs.map((job) => (
+        <CVJob
+          jobTitle={job.jobTitle}
+          company={job.company}
+          duration={job.duration}
+          jobDescription={job.jobDescription}
+          logo={job.logo}
+        />
+      ))}
+    </div>
+    <div className="py-4 border-b border-gray-700">
+      <h3 className="text-2xl font-semibold">education</h3>
+    </div>
   </div>
 );
