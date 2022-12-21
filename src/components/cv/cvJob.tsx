3509c89d951b96a3090dc70d73f167a18f2373e8
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2022-12-21 19:21:40 +0100

    Add link on img

--- src/components/cv/cvJob.tsx
+++ src/components/cv/cvJob.tsx
@@ -2,23 +2,23 @@ import { useState } from 'react';
 import { CvDescription } from './cvDescription';
 
 export interface CVJobProps {
-  jobTitle: string;
   company: string;
   duration: string;
+  jobTitle: string;
+  href?: string;
   jobDescription: string;
   logo: string;
   location: string;
-  href?: string;
 }
 
 export const CVJob: React.FC<CVJobProps> = ({
-  jobTitle,
   company,
   duration,
+  href = '',
+  jobTitle,
   jobDescription,
   logo,
   location,
-  href = 'test',
 }) => {
   const [displayDescription, setDisplayDescription] = useState(false);
 
@@ -31,8 +31,8 @@ export const CVJob: React.FC<CVJobProps> = ({
   return (
     <div className="my-4">
       <div className="flex justify-between">
-        <div className="pr-4">
-          <h4 className="font-semibold text-xl my-2">
+        <div>
+          <h4 className="font-semibold text-xl">
             {jobTitle} at {company}
           </h4>
           <span className="text-gray-600 my-2">
@@ -40,7 +40,7 @@ export const CVJob: React.FC<CVJobProps> = ({
           </span>
           <CvDescription displayDescription={displayDescription} setDisplayDescription={setDisplayDescription} />
         </div>
-        <img src={logo} alt="logo" className="block w-24 h-24 mt-2 m-2 cursor-pointer" onClick={handleOnClick} />
+        <img src={logo} alt="logo" className="w-24 h-24 mx-2 cursor-pointer" onClick={handleOnClick} />
       </div>
       {displayDescription && <p className="text-gray-800 text-justify bg-gray-100 p-4 mt-4">{jobDescription}</p>}
     </div>
