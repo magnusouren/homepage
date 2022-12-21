b943e72342f41190be7df7fc1ab3372935a0fa8e
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2022-12-21 19:20:16 +0100

    Refactor

--- src/components/home/wallpaper.tsx
+++ src/components/home/wallpaper.tsx
@@ -1,3 +1,3 @@
 import ImageToAdd from '../../img/monaco.jpg';
 
-export const Wallpaper = () => <img alt="Monaco swimminpool" src={ImageToAdd} className="min-w-full"></img>;
+export const Wallpaper = () => <img alt="Monaco swimminpool" src={ImageToAdd} className="min-w-full" />;
