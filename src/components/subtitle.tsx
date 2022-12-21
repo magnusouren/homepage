89301ec95c6c9983dc0277632fa3bc5875b39720
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2022-12-21 22:28:57 +0100

    Refactor props

--- src/components/subtitle.tsx
+++ src/components/subtitle.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
-
 type SubtitleProps = {
   subtitle: string;
 };
 
-export const Subtitle: React.FC<SubtitleProps> = ({ subtitle }) => (
-  <h2 className="text-4xl font-bold my-4">{subtitle}</h2>
-);
+export const Subtitle = ({ subtitle }: SubtitleProps) => <h2 className="text-4xl font-bold my-4">{subtitle}</h2>;
