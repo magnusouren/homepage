bd267c51745f7c7d9f46b71ad244c4d1cc09f5e5
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2022-12-21 22:08:10 +0100

    Add month and years

--- src/data/cv/cvJobs.ts
+++ src/data/cv/cvJobs.ts
@@ -7,7 +7,8 @@ export const CVJobsData: CVJobProps[] = [
   {
     jobTitle: 'Webshop maintainer and developer',
     company: 'Havsport Diving center AS',
-    duration: 'January 2022 - Present',
+    startYear: 2022,
+    startMonth: 'January',
     jobDescription:
       "As part of my job at Havsport Diving Center AS, I was responsible for maintaining and developing the company's webshop. This involved updating and adding new products, as well as making price adjustments based on market conditions. I worked to ensure that the webshop was user-friendly and provided a smooth online shopping experience for our customers. Overall, my role was critical in maintaining the online presence and success of Havsport Diving Center AS.",
     logo: Havsport,
@@ -17,7 +18,8 @@ export const CVJobsData: CVJobProps[] = [
   {
     jobTitle: 'Lifeguard',
     company: 'Norwegian Lifeguard Association',
-    duration: 'June 2022 - August 2022',
+    startYear: 2022,
+    startMonth: 'June',
     jobDescription:
       'Summer job as lifeguard at the public beaches during the summer season in Oslo. Lifeguarding and first aid for the visitors at the beach. Preventing accidents and helping people in need, both in and out of the water. Internationally certified lifeguard.',
     logo: NLS,
@@ -27,7 +29,10 @@ export const CVJobsData: CVJobProps[] = [
   {
     jobTitle: 'Diving instructor',
     company: 'The Norwegain Naval Diving School - Royal Norwegian Navy',
-    duration: 'July 2021 - July 2022',
+    startYear: 2020,
+    startMonth: 'July',
+    endYear: 2021,
+    endMonth: 'July',
     jobDescription:
       'As a diving instructor for the Royal Norwegian Navy, I was responsible for teaching and training a wide range of diving skills to members of the Norwegian Armed Forces. This included providing instruction in various diving techniques, as well as teaching safety protocols and emergency procedures. I worked with divers of all levels, from beginners to experienced professionals. In addition to conducting in-water training, I participated in the development of the instructional materials and lesson plans, and assisted in the maintenance and upkeep of diving equipment. Overall, my role was to ensure that the Norwegian Armed Forces had highly skilled and proficient divers.',
     logo: ForsvaretLogo,
@@ -37,7 +42,10 @@ export const CVJobsData: CVJobProps[] = [
   {
     jobTitle: 'Shipdiver',
     company: 'The Norwegain Naval Diving School - Royal Norwegian Navy',
-    duration: 'July 2019 - July 2021',
+    startYear: 2019,
+    startMonth: 'July',
+    endYear: 2020,
+    endMonth: 'July',
     jobDescription:
       'Served as private as shipdiver for the Royal Norwegian Navy. Helping with maintanance and as instructor at the diving school. Teaching and training divers with fundamental skills for becoming shipdivers.',
     logo: ForsvaretLogo,
