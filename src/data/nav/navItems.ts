63fd690adf2f4c50e17562dc66b1489c2cc3f7b3
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2022-12-25 21:49:08 +0100

    Add navitems in datafile

---
+++ src/data/nav/navItems.ts
@@ -0,0 +1,46 @@
+import { navItemType } from '../../components/nav/navItem';
+
+export const navItems: navItemType[] = [
+  {
+    text: 'home',
+    emoji: '🤍',
+    url: '',
+    target: '_self',
+  },
+  {
+    text: 'about me',
+    emoji: '👀',
+    url: 'me',
+    target: '_self',
+  },
+  {
+    text: 'CV',
+    emoji: '📖',
+    url: 'cv',
+    target: '_self',
+  },
+  {
+    text: 'projects',
+    emoji: '💾',
+    url: 'projects',
+    target: '_self',
+  },
+  {
+    text: 'github',
+    emoji: '💻',
+    url: 'https://www.github.com/magnusouren',
+    target: '_blank',
+  },
+  {
+    text: 'linkedin',
+    emoji: '🔗',
+    url: 'https://www.linkedin.com/in/magnus-tomter-ouren-44a80321b/',
+    target: '_blank',
+  },
+  {
+    text: 'email',
+    emoji: '✉️',
+    url: 'mailto:magnut0203@gmail.com',
+    target: '_self',
+  },
+];
