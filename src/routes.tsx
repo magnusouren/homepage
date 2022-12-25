7bede33c94a8367dbcaed96b89e416ab616483ed
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2022-12-25 21:22:47 +0100

    Add hash routing

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,11 @@
-import { createRoutesFromElements, createBrowserRouter, Route } from 'react-router-dom';
+import { createRoutesFromElements, createBrowserRouter, Route, createHashRouter } from 'react-router-dom';
 import { PageLayout } from './layouts/page';
 import { AboutMe } from './pages/aboutMe';
 import { CV } from './pages/cv';
 import { HomeView } from './pages/homeview';
 import { Projects } from './pages/projects';
 
-export const router = createBrowserRouter(
+export const router = createHashRouter(
   createRoutesFromElements(
     <Route path="/" element={<PageLayout />}>
       <Route path="/" element={<HomeView />} />
