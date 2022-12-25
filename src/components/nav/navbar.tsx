78f6a67b061c49400a616e42fe185826dfa4ff75
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2022-12-25 21:49:42 +0100

    Remove data and update rednering

--- src/components/nav/navbar.tsx
+++ src/components/nav/navbar.tsx
@@ -1,62 +1,14 @@
-import { Link } from 'react-router-dom';
+import { navItems } from '../../data/nav/navItems';
+import { DesktopNav } from './desktopNav';
 import { MobileNav } from './mobileNav';
-import { NavItem, navItemType } from './navItem';
-
-const navItems: navItemType[] = [
-  {
-    text: 'home',
-    emoji: '🤍',
-    url: '',
-    target: '_self',
-  },
-  {
-    text: 'about me',
-    emoji: '👀',
-    url: 'me',
-    target: '_self',
-  },
-  {
-    text: 'CV',
-    emoji: '📖',
-    url: 'cv',
-    target: '_self',
-  },
-  {
-    text: 'projects',
-    emoji: '💾',
-    url: 'projects',
-    target: '_self',
-  },
-  {
-    text: 'github',
-    emoji: '💻',
-    url: 'https://www.github.com/magnusouren',
-    target: '_blank',
-  },
-  {
-    text: 'linkedin',
-    emoji: '🔗',
-    url: 'https://www.linkedin.com/in/magnus-tomter-ouren-44a80321b/',
-    target: '_blank',
-  },
-  {
-    text: 'email',
-    emoji: '✉️',
-    url: 'mailto:magnut0203@gmail.com',
-    target: '_self',
-  },
-];
 
 export const Navbar = () => (
-  <nav className="w-full md:w-48 md:h-screen bg-black fixed left-0 top-0 text-white">
+  <nav className="w-full md:w-48 md:min-h-screen bg-black fixed left-0 top-0 text-white">
     <div className="hidden md:block">
-      <ul>
-        {navItems.map((navItem) => (
-          <NavItem text={navItem.text} url={navItem.url} target={navItem.target} emoji={navItem.emoji} />
-        ))}
-      </ul>
-      <p className="hidden md:block absolute bottom-8 w-full text-center font-extralight align-middle">- 2022 -</p>
+      <DesktopNav navItems={navItems} />
     </div>
-    <MobileNav navItems={navItems} />
+    <div className="block md:hidden">
+      <MobileNav navItems={navItems} />
+    </div>
   </nav>
 );
