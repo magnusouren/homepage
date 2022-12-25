697eacd5d2d1afeaa0d5de77fa7c866e89c28825
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2022-12-25 21:49:24 +0100

    As desktopNav as component

---
+++ src/components/nav/desktopNav.tsx
@@ -0,0 +1,16 @@
+import { NavItem, navItemType } from './navItem';
+
+type DesktopNavProps = {
+  navItems: navItemType[];
+};
+
+export const DesktopNav = ({ navItems }: DesktopNavProps) => (
+  <>
+    <ul>
+      {navItems.map((navItem) => (
+        <NavItem text={navItem.text} url={navItem.url} target={navItem.target} emoji={navItem.emoji} />
+      ))}
+    </ul>
+    <p className="hidden md:block absolute bottom-8 w-full text-center font-extralight align-middle">- 2022 -</p>
+  </>
+);
