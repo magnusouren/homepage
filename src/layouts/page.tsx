1819a9d554f90b3e56536c2f33d2ef3020ac1737
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2022-12-26 18:04:36 +0100

    Add footer

--- src/layouts/page.tsx
+++ src/layouts/page.tsx
@@ -1,12 +1,16 @@
 import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
+import { Footer } from '../components/footer/footer';
 import { Navbar } from '../components/nav/navbar';
 
 export const PageLayout: FC = () => (
   <main>
     <Navbar />
-    <div className="ml-0 mt-24 md:pl-48 md:mt-0 mb-20 w-full">
+    <div className="ml-0 mt-24 md:pl-48 md:mt-0 mb-20 w-full min-h-screen">
       <Outlet />
+    </div>
+    <div className="md:hidden">
+      <Footer />
     </div>
   </main>
 );
