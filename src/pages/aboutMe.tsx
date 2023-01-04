ddd9c8fbab88f91fe50f4ca5af75bbdaf449e934
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2023-01-04 13:20:17 +0100

    Fix typos

--- src/pages/aboutMe.tsx
+++ src/pages/aboutMe.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { CvPhoto } from '../components/aboutme/portrait';
 import { Subtitle } from '../components/subtitle';
 
@@ -6,22 +7,30 @@ export const AboutMe = () => (
     <div className="ml-auto mr-auto max-w-xl px-4">
       <Subtitle subtitle="about me" />
       <p className="my-4 text-justify">
-        I am a 22 year old student from Trondheim, Norway. Grew up and comes from Oslo. Currently studying informatics
-        at NTNU in Trondheim. At the moment at the second year of the bachelor program. I am a very curious person and I
-        love to learn new things. Studying informatics makes it posible to learn a lot of new things I never thought
-        would be relevant to learn, but the more I learn the more I understands how much I don't know.
+        I am a 22 year old student from Trondheim, Norway. I grew up and come from Oslo. Currently, I am studying
+        informatics at NTNU in Trondheim. At the moment, I am in the second year of the bachelor program. I am a very
+        curious person and I love to learn new things. Studying informatics makes it possible for me to learn a lot of
+        new things that I never thought would be relevant to learn, but the more I learn, the more I understand how much
+        I don't know.
       </p>
       <CvPhoto />
       <p className="my-4 text-justify">
-        Love the sosial aspect of studying in Trondheim, and I am very happy to be a part of the student life here.
-        Currently I am a part of the student organization for informatics, called Online. Where I am also a part of the
-        board for the student organization. My role in the board is to be represant for the Business relation committee.
-        More about Online can be found{' '}
+        I love the social aspect of studying in Trondheim, and I am very happy to be a part of the student life here.
+        Currently, I am a part of the student organization for informatics, called Online. I am also a part of the board
+        for the student organization. My role in the board is to represent the Business relation committee. More about
+        Online can be found.{' '}
         <a href="https://online.ntnu.no/" className="underline hover:text-gray-500">
           here
         </a>
-        . If you want to know more about me and what I do you should visit the <i className="italic">CV</i> and{' '}
-        <i className="italic">projects</i> page.
+        . If you want to know more about me and what I do you should visit the{' '}
+        <Link to="/cv">
+          <i className="italic underline hover:text-gray-500">CV</i>
+        </Link>{' '}
+        and{' '}
+        <Link to="/projects">
+          <i className="italic underline hover:text-gray-500">projects</i>
+        </Link>{' '}
+        page.
       </p>
       <p className="my-4 text-justify">
         Currently I am working for{' '}
--- src/pages/homeview.tsx
+++ src/pages/homeview.tsx
@@ -8,22 +8,22 @@ export const HomeView = () => (
       <h2 className="text-4xl font-bold">magnus.ouren.no</h2>
       <p className="font-semibold text-gray-500">react/typescript/tailwind css</p>
       <p className="mt-8 mb-4 text-justify">
-        Hello I'm Magnus and this is my first homepage. I am currently studying informatics at my second year at NTNU in
-        Trondheim. This webpage is made with React, Typescript and Tailwind CSS as a hoby project to learn more about
-        new technologies. It started as a workshop with a friend for learning React, but has developed into a bigger
-        webpage about me, what I do, and I who am. that's reasonably selfish, but hopefully I wil add other types of
-        content in the future.
+        Hello, I'm Magnus and this is my first homepage. I am currently studying informatics at my second year at NTNU
+        in Trondheim. This webpage is made with React, Typescript, and Tailwind CSS as a hobby project to learn more
+        about new technologies. It started as a workshop with a friend for learning React, but has developed into a
+        bigger webpage about me, what I do, and who I am. That's reasonably selfish, but hopefully I will add other
+        types of content in the future.
       </p>
       <p className="my-4 text-justify">
-        It's my first ever buildt react webpage. My react-skills are mostly self-taught, so don't be harsh. You can
-        check out the repository for this webpage{' '}
+        It's my first ever built React webpage. My React skills are mostly self-taught, so don't be harsh. You can check
+        out the repository for this webpage.{' '}
         <a target="blank" href="https://github.com/magnusouren/homepage" className="underline hover:text-gray-500">
           here!
         </a>
       </p>
       <p className="my-4 text-justify">
         The amazing photo you can see above was taken on my last holiday. The picture was taken in Monaco, where I was
-        on a trip with my friends. Expensive stay, but a very cool place! Can be strongly recommended!
+        on a trip with my friends. It was an expensive stay, but a very cool place! I can strongly recommend it!
       </p>
     </div>
   </div>
