26f3cdf8ff71d742539c310dd72f29ed335b084b
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2023-01-17 22:21:12 +0100

    Remove console log

--- src/components/projects/readMore.tsx
+++ src/components/projects/readMore.tsx
@@ -3,23 +3,20 @@ interface ReadMoreProps {
   secondaryImages?: string[];
 }
 
-export const ReadMore = ({ longDescription, secondaryImages = [] }: ReadMoreProps) => {
-  console.log(secondaryImages);
-  return (
-    <div className="text-gray-800 text-justify bg-gray-100 p-4 mt-4">
-      <p>{longDescription}</p>
-      {secondaryImages && (
-        <div className="flex flex-wrap justify-center">
-          {secondaryImages.map((image) => (
-            <img
-              src={image}
-              alt="alt text
+export const ReadMore = ({ longDescription, secondaryImages = [] }: ReadMoreProps) => (
+  <div className="text-gray-800 text-justify bg-gray-100 p-4 mt-4">
+    <p>{longDescription}</p>
+    {secondaryImages && (
+      <div className="flex flex-wrap justify-center">
+        {secondaryImages.map((image) => (
+          <img
+            src={image}
+            alt="alt text
                 "
-              className="my-2 md:w-1/2 sm:w-full"
-            />
-          ))}
-        </div>
-      )}
-    </div>
-  );
-};
+            className="my-2 md:w-1/2 sm:w-full"
+          />
+        ))}
+      </div>
+    )}
+  </div>
+);
