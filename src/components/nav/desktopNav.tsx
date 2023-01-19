e9af547a1097ea8cbebfa3f03ea878d1c058cc69
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2023-01-19 20:39:30 +0100

    Add dynamic date

--- src/components/nav/desktopNav.tsx
+++ src/components/nav/desktopNav.tsx
@@ -4,19 +4,25 @@ type DesktopNavProps = {
   navItems: navItemType[];
 };
 
-export const DesktopNav = ({ navItems }: DesktopNavProps) => (
-  <>
-    <ul>
-      {navItems.map((navItem) => (
-        <NavItem
-          text={navItem.text}
-          url={navItem.url}
-          target={navItem.target}
-          emoji={navItem.emoji}
-          key={navItem.text}
-        />
-      ))}
-    </ul>
-    <p className="hidden md:block absolute bottom-8 w-full text-center font-extralight align-middle">- 2022 -</p>
-  </>
-);
+export const DesktopNav = ({ navItems }: DesktopNavProps) => {
+  const year = new Date().getFullYear();
+
+  return (
+    <>
+      <ul>
+        {navItems.map((navItem) => (
+          <NavItem
+            text={navItem.text}
+            url={navItem.url}
+            target={navItem.target}
+            emoji={navItem.emoji}
+            key={navItem.text}
+          />
+        ))}
+      </ul>
+      <p className="hidden md:block absolute bottom-8 w-full text-center font-extralight align-middle">
+        {'- ' + year + ' -'}
+      </p>
+    </>
+  );
+};
