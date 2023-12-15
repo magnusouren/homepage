1d48c651b9f2a1bc8d7960db9935dd4106b950cb
magnusouren <magnut0203@gmail.com>
2023-12-15 14:29:47 +0100

    Add ProjectProps interface to src/types.ts

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface ProjectProps {
+  title: string;
+  image: string;
+  shortDescription: string;
+  longDescription: string;
+  tags: string[];
+  url?: string;
+  secondaryImages?: string[];
+}
