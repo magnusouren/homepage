5f072d52c4e2029fe0968473496e4c3796fdb99f
magnusouren <magnut0203@gmail.com>
2024-06-02 14:13:17 +0200

    responsive timeline

--- src/components/timeline/timeOject.tsx
+++ src/components/timeline/timeOject.tsx
@@ -40,10 +40,10 @@ export const TimeObject: FC<TimeObjectProps> = ({
 
   return (
     <div
-      className={`flex items-center ${index % 2 === 0 ? 'justify-start' : 'justify-end'}`}
+      className={`flex items-center justify-end ${index % 2 === 0 ? 'md:justify-start' : 'md:justify-end'}`}
     >
       <div
-        className={`relative w-1/2 p-4 ${index % 2 === 0 ? 'text-right' : 'text-left'}`}
+        className={`relative w-3/4 md:w-1/2 p-4 ${index % 2 === 0 ? 'md:text-right' : 'md:text-left'}`}
       >
         <div
           className={`${iconType[type].color} p-4 rounded-lg shadow-md border border-gray-200 mx-4 relative ${content ? 'cursor-pointer  hover:shadow-lg' : ''}`}
@@ -64,12 +64,12 @@ export const TimeObject: FC<TimeObjectProps> = ({
           )}
         </div>
         <div
-          className={`absolute top-1/2 transform -translate-y-1/2 ${index % 2 === 0 ? '-right-5' : '-left-5'}  h-10 w-10 bg-white rounded-full border text-center border-gray-200 shadow-md flex items-center justify-center`}
+          className={`absolute top-1/2 transform -translate-y-1/2 -left-24  ${index % 2 === 0 ? 'md:left-auto md:-right-6' : 'md:-left-4'}  h-10 w-10 bg-white rounded-full border text-center border-gray-200 shadow-md flex items-center justify-center`}
         >
           {iconType[type].tag}
         </div>
         <div
-          className={`absolute top-1/2 transform -translate-y-1/2 ${index % 2 === 0 ? '-right-48 justify-start pl-4' : '-left-48 justify-end pr-4'}  h-8 w-40 flex items-center  text-gray-600 left`}
+          className={`absolute top-1/2 transform -translate-y-1/2 -left-12 w-16 md:w-32 text-center ${index % 2 === 0 ? 'md:-right-40 md:justify-start md:pl-4 md:left-auto' : 'md:-left-40 md:justify-end md:pr-4'}  h-8 w-40 flex items-center  text-gray-600 left`}
         >
           {monthToString(startTime.month)} {startTime.year}
         </div>
